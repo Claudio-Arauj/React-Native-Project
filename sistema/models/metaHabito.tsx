6dff8abbad8a9a11ef07d6c7c4ae0332863796b2
Bruno Costa Souto <brunocostasoutomoto2005@gmail.com>
2025-06-08 17:16:52 -0300

    feat: Criar Hábitos - Dados locais

--- sistema/models/metaHabito.tsx
+++ sistema/models/metaHabito.tsx
@@ -5,6 +5,7 @@ export class MetaHabito {
   frequencia: 'diária' | 'semanal';
   lembreteHorario?: string;
   criadoEm: Date;
+  cor?: string // <= nova propriedade
 
   constructor(
     id: string,
@@ -12,7 +13,8 @@ export class MetaHabito {
     nome: string,
     frequencia: 'diária' | 'semanal',
     criadoEm: Date,
-    lembreteHorario?: string
+    lembreteHorario?: string,
+    cor?: string // <= nova propriedade
   ) {
     this.id = id;
     this.usuarioId = usuarioId;
@@ -20,6 +22,7 @@ export class MetaHabito {
     this.frequencia = frequencia;
     this.criadoEm = criadoEm;
     this.lembreteHorario = lembreteHorario;
+    this.cor = cor;
   }
 
   static fromDb(row: {
@@ -28,6 +31,7 @@ export class MetaHabito {
     nome: string;
     frequencia: 'diária' | 'semanal';
     lembrete_horario?: string | null;
+    cor?: string | null;
     criado_em: string;
   }): MetaHabito {
     return new MetaHabito(
@@ -36,7 +40,8 @@ export class MetaHabito {
       row.nome,
       row.frequencia,
       new Date(row.criado_em),
-      row.lembrete_horario ?? undefined
+      row.lembrete_horario ?? undefined,
+      row.cor ?? undefined
     );
   }
 }
